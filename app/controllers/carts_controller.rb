52c6c164df6dc54b8c858d486c544b761d88f58a
mwm0022 <mwm0022@qq.com>
2015-10-23 15:43:27 +0600

    提交10月23日

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -10,6 +10,12 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'Invalid cart'
+    end
   end
 
   # GET /carts/new
@@ -64,7 +70,12 @@ class CartsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
-      @cart = Cart.find(params[:id])
+        begin
+            @cart = Cart.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+            logger.error "Attempt to access invalid cart #{params[:id]}"
+            redirect_to store_url, notice: 'Invalid cart'
+        end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
