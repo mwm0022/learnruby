42c6feabf128cf74d834bc8b14f64d402443805a
mwm0022 <mwm0022@qq.com>
2015-10-30 15:40:50 +0600

    10月30日提交

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ActiveRecord::Base
 	PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order"]
+	validates :name, :address, :emall, :pay_type, :prensence => true
+	validates :pay_type, :inclusion => PAYMENT_TYPES
 end
